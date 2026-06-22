f9bd63c19f4ca648a88047a7be7c14cdf92d11f4
skar92 <135007671+skar92@users.noreply.github.com>
2026-06-22 12:33:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ porra_futbolistas = {
     'Sierra': {'Kane': 2, 'Julián Álvarez': 0},
     'Joaquín': {'Messi': 3, 'Olise': 0},
     'Ejkar': {'Lautaro': 0, 'Raphinha': 0},
-    'Vecina': {'Havertz': 2, 'Lamine Yamal': 0},
+    'Vecina': {'Havertz': 2, 'Lamine Yamal': 1},
     'Telenti': {'Endrick': 0, 'Ramos': 0},
     'Miguel Ángel': {'Haaland': 2, 'Embolo': 1},
     'Mírete': {'Oyarzabal': 2, 'El Bicho': 0}, 
